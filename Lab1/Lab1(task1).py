349a8c37bd451ffc4ecad1f20f48f72a561c8182
Кiрiлл Даниленко <52251652+kdanylenko@users.noreply.github.com>
2019-11-15 01:04:13 +0300

    kl;lk;lk;k;llk;k

--- Lab1/Lab1(task1).py
+++ Lab1/Lab1(task1).py
@@ -1,12 +1,4 @@
 # 1 task
 import re
 i = 1
-word = input("Enter your string:")
-result = re.split(r'[B-Zb-z]', word)
-if len(result)==1:
-    for i in range(5):
-        for i in range(8):
-            print(result[0], end="")
-        print()
-else:
-    print("Incorrect string")
+ok;luliouloilk;kl;l;lk;lk;k;kl;kl;
